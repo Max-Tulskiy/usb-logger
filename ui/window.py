f2bb823b6b6886e91362f17eb83fc0319d61385f
Maxim Tulskiy <maks.tulskiy.04@gmail.com>
2025-04-06 17:31:16 +0300

    Update ui

--- ui/window.py
+++ ui/window.py
@@ -3,7 +3,7 @@
 ################################################################################
 ## Form generated from reading UI file 'untitled.ui'
 ##
-## Created by: Qt User Interface Compiler version 6.8.3
+## Created by: Qt User Interface Compiler version 6.9.0
 ##
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
@@ -15,9 +15,9 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QHeaderView, QMainWindow, QMenuBar,
-    QPushButton, QSizePolicy, QStatusBar, QTableWidget,
-    QTableWidgetItem, QWidget)
+from PySide6.QtWidgets import (QApplication, QDateTimeEdit, QHeaderView, QLabel,
+    QMainWindow, QMenuBar, QPushButton, QSizePolicy,
+    QStatusBar, QTableWidget, QTableWidgetItem, QWidget)
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
@@ -31,7 +31,54 @@ class Ui_MainWindow(object):
         self.log_table.setGeometry(QRect(15, 91, 761, 411))
         self.sort_button = QPushButton(self.centralwidget)
         self.sort_button.setObjectName(u"sort_button")
-        self.sort_button.setGeometry(QRect(680, 50, 91, 31))
+        self.sort_button.setGeometry(QRect(690, 30, 91, 31))
+        self.sort_button.setStyleSheet(u"QPushButton {\n"
+"border-radius: 5px;\n"
+"border: 1px solid black;\n"
+"}\n"
+"\n"
+"QPushButton:pressed {\n"
+"    color: white;               \n"
+"    background-color: #1c5980;\n"
+"}")
+        self.filter_button = QPushButton(self.centralwidget)
+        self.filter_button.setObjectName(u"filter_button")
+        self.filter_button.setGeometry(QRect(490, 30, 81, 31))
+        self.filter_button.setStyleSheet(u"QPushButton {\n"
+"border-radius: 5px;\n"
+"border: 1px solid black;\n"
+"}\n"
+"\n"
+"QPushButton:pressed {\n"
+"    color: white;               \n"
+"    background-color: #1c5980;\n"
+"}")
+        self.export_button = QPushButton(self.centralwidget)
+        self.export_button.setObjectName(u"export_button")
+        self.export_button.setGeometry(QRect(590, 30, 81, 31))
+        self.export_button.setStyleSheet(u"QPushButton {\n"
+"border-radius: 5px;\n"
+"border: 1px solid black;\n"
+"}\n"
+"\n"
+"QPushButton:pressed {\n"
+"    color: white;               \n"
+"    background-color: #1c5980;\n"
+"}")
+        self.label = QLabel(self.centralwidget)
+        self.label.setObjectName(u"label")
+        self.label.setGeometry(QRect(30, 10, 31, 17))
+        self.label_2 = QLabel(self.centralwidget)
+        self.label_2.setObjectName(u"label_2")
+        self.label_2.setGeometry(QRect(30, 40, 31, 17))
+        self.end_date = QDateTimeEdit(self.centralwidget)
+        self.end_date.setObjectName(u"end_date")
+        self.end_date.setGeometry(QRect(70, 40, 194, 26))
+        self.end_date.setStyleSheet(u"")
+        self.start_date = QDateTimeEdit(self.centralwidget)
+        self.start_date.setObjectName(u"start_date")
+        self.start_date.setGeometry(QRect(70, 10, 194, 26))
+        self.start_date.setStyleSheet(u"")
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(MainWindow)
         self.menubar.setObjectName(u"menubar")
@@ -48,6 +95,10 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
-        self.sort_button.setText(QCoreApplication.translate("MainWindow", u"\u0421\u043e\u0440\u0442\u0438\u0440\u043e\u0432\u0430\u0442\u044c", None))
+        self.sort_button.setText(QCoreApplication.translate("MainWindow", u"sort", None))
+        self.filter_button.setText(QCoreApplication.translate("MainWindow", u"filter", None))
+        self.export_button.setText(QCoreApplication.translate("MainWindow", u"exp2pdf", None))
+        self.label.setText(QCoreApplication.translate("MainWindow", u"start", None))
+        self.label_2.setText(QCoreApplication.translate("MainWindow", u"end", None))
     # retranslateUi
 
